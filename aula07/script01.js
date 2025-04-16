60ddcb2d650661bb20d17889ec4d611869b3f8af
AnnaManuel <161508845+AnnaBeatrizSuritaManuel@users.noreply.github.com>
2025-04-16 16:59:07 -0300

    Add files via upload

---
+++ aula07/script01.js
@@ -0,0 +1,7 @@
+let titulo = document.getElementById("titulo");
+let imagem = document.getElementById("imagem");
+titulo.innerText = "Olá pessoa";
+
+titulo.style.color = "purple";
+
+imagem.src = "img/sigwine.jpg";
---
+++ aula07/script02.js
@@ -0,0 +1,7 @@
+let botao = document.getElementById("botao");
+
+function mostrar_alert(){
+    alert("Você clicou o botão");
+    botao.innerHTML = "Foi clicado <3";
+}
+ 
